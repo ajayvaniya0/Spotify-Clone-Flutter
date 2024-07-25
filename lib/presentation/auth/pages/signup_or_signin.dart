268958f288b54abda7c79a237dda411b5b36157c
Ajay <ajayvaniya54@gmail.com>
2024-07-25 12:07:51 +0530

    Signup and Signin Pages UI

--- lib/presentation/auth/pages/signup_or_signin.dart
+++ lib/presentation/auth/pages/signup_or_signin.dart
@@ -5,6 +5,8 @@ import 'package:spotify/common/widgets/button/basic_app_button.dart';
 import 'package:spotify/core/configs/assets/app_images.dart';
 import 'package:spotify/core/configs/assets/app_vectors.dart';
 import 'package:spotify/core/configs/theme/app_colors.dart';
+import 'package:spotify/presentation/auth/pages/signin.dart';
+import 'package:spotify/presentation/auth/pages/signup.dart';
 
 import '../../../common/widgets/appbar/app_bar.dart';
 
@@ -78,7 +80,14 @@ class SignupOrSigninPage extends StatelessWidget {
                       Expanded(
                         flex: 1,
                         child: BasicAppButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                      const SignupPage()),
+                            );
+                          },
                           title: 'Register',
                         ),
                       ),
@@ -88,7 +97,15 @@ class SignupOrSigninPage extends StatelessWidget {
                       Expanded(
                         flex: 1,
                         child: TextButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    const SigninPage(),
+                              ),
+                            );
+                          },
                           child: Text(
                             'Sign in',
                             style: TextStyle(
